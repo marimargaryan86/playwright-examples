bedf81d652c64ba6a80a7f43366dd5d636357f4d
admin <admin@TeamCity>
2025-11-14 13:53:32 +0400

    runners of 'ttttttt' build configuration were updated (TeamCity change in 'Playwright Examples' project)

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,5 +1,6 @@
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
 
@@ -58,6 +59,14 @@ object Ttttttt : BuildType({
 
     vcs {
         root(HttpsGithubComMarimargaryan86testTw94936git)
+    }
+
+    steps {
+        script {
+            name = "sleep"
+            id = "sleep"
+            scriptContent = "sleep 10"
+        }
     }
 })
 
