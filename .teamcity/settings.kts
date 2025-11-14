7196b3aa1457bbe26cace2fd1aee41da59d8cbed
admin <admin@TeamCity>
2025-11-14 15:24:08 +0000

    'https://github.com/marimargaryan86/brbr.git' VCS root was created (TeamCity change in 'Playwright Examples (5)' project)

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -30,6 +30,7 @@ version = "2025.07"
 
 project {
 
+    vcsRoot(HttpsGithubComMarimargaryan86brbrGit)
     vcsRoot(HttpsGithubComMarimargaryan86testTw94936gitRefsHeadsMain)
     vcsRoot(HttpsGithubComMarimargaryan86testTw94936git)
 
@@ -78,6 +79,15 @@ object Build1 : BuildType({
     features {
         perfmon {
         }
+    }
+})
+
+object HttpsGithubComMarimargaryan86brbrGit : GitVcsRoot({
+    name = "https://github.com/marimargaryan86/brbr.git"
+    url = "https://github.com/marimargaryan86/brbr.git"
+    branch = "refs/heads/main"
+    authMethod = password {
+        userName = "marimargaryan86"
     }
 })
 
